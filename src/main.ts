85f5adf07b567ec6a59bf701c0dbd467179f2943
Long Le <levanlong.91@gmail.com>
2021-04-23 23:18:59 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('Enrich Skills API')
+    .setDescription('Support Integration With Front End')
+    .setVersion('1.0')
+    .addTag('User Management')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
